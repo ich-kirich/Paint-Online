58b03b16ba6f2b37c77413f3decfbf45a525d443
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2023-12-09 17:14:23 +0300

    fix: correct path to favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Paint Online</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./favicon.ico" />
       </Head>
       <StyledEngineProvider injectFirst>
         <MainPage />
